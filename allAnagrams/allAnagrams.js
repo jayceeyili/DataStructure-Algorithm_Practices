9c7bcb917a63539f12e378cc234490fee5947f8b
Li Yi <jayceeyili@gmail.com>
2017-05-22 23:28:46 -0700

    complete allAnagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,27 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
+// var allAnagrams = function(string) {
+//   // Your code here.
+// };
+
+const allAnagrams = string => {
+  let result = {};
+
+  let recusive = (anagram, str) => {
+    let strArr = str.split('');
+    if (!str) {
+      result[anagram] = 1;
+    }
+
+    strArr.forEach((char, i) => {
+      recusive(anagram + char, str.slice(0, i) + str.slice(i + 1));
+    })
+  }
+
+  recusive('', string);
+
+  return Object.keys(result);
 };
+
+console.log(allAnagrams('aabbc'));
