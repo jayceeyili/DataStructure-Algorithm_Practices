9b2c01e347cc9102552d17a0c87451af9f952b05
Li Yi <jayceeyili@gmail.com>
2017-05-31 14:22:46 -0700

    complete composePipe

--- composePipe/composePipe.js
+++ composePipe/composePipe.js
@@ -1,7 +1,7 @@
 /*
  * Write Compose and Pipe functions.
- * 
- * Step 1: Implement the function Compose: 
+ *
+ * Step 1: Implement the function Compose:
  *
  * Compose should return a function that is the composition of a list of
  * functions of arbitrary length.
@@ -19,11 +19,11 @@
  * Step 2: Implement the function Pipe:
  *
  * Pipe composes a series of functions and returns the resulting function.
- * 
+ *
  * Each function is called on the return value of the preceding function.
  *
  * You can view pipe as moving left to right through its arguments.
- * 
+ *
  * Pipe Example:
  *  var add2 = function(number){ return number + 2; }
  *  var multiplyBy3 = function(number){ return number * 3; }
@@ -31,10 +31,24 @@
  *  pipe(add2, multiplyBy3, multiplyBy3)(5) // 63
  */
 
-'use strict';
+// 'use strict';
 
-var compose = function() {
-};
+// var compose = function() {
+// };
+//
+// var pipe = function() {
+// };
 
-var pipe = function() {
-};
+const compose = (...functions) => input => functions.reduceRight((a, b) => b(a), input);
+
+const pipe = (...functions) => input => functions.reduce((a, b) => b(a), input);
+
+// var greet = function(name){ return 'hi: ' + name;}
+// var exclaim = function(statement) { return statement.toUpperCase() + '!';}
+// var welcome = compose(greet, exclaim);
+// console.log(welcome('phillip'));
+//
+// var add2 = function(number){ return number + 2; }
+// var multiplyBy3 = function(number){ return number * 3; }
+// console.log(pipe(add2, multiplyBy3)(5)); // 21);
+// console.log(pipe(add2, multiplyBy3, multiplyBy3)(5)); // 63);
